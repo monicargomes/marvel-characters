ba00a0dc78697fa182f1895ff717b79c357731d0
Monica Gomes <monicargomes@gmail.com>
2018-02-09 16:50:25 +0100

    Add animation to scroll to the top when a character on the slider is clicked

--- js/featured-character.js
+++ js/featured-character.js
@@ -2,4 +2,7 @@ $('.small-img').click(function(){
   var imageName = $(this).attr('src').slice(9);
 
   $('.large-img').attr('src', 'img/large' + imageName);
+
+  $('html').animate({ scrollTop: 0 }, "slow");
+
 })
